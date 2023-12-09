cb6937a334227e1ce5ea3777aa044009d962ee74
Semanur Çakır <semanurcakir11@gmail.com>
2023-12-09 21:09:27 +0300

    Code has been debugged

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const ejs=require('ejs')
 const path = require('path');
 
 const app = express();
 
+//template engine
+app.set("view engine", "ejs");
+
 //MIDDLEWARES
 
 app.use(express.static('public'));
-
+//ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("index")
+});
+app.get('/about', (req, res) => {
+  res.render("about")
+});
+app.get('/add', (req, res) => {
+  res.render("add")
 });
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Sunucu ${port}'de başatıldı.`);
+  console.log(`Sunucu ${port}'de başlatildi.`);
 });
